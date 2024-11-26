084865d347e1a0746bd2753ebdc83a0b460b9cec
Steve <mas.ste@gmail.com>
2024-11-26 21:08:06 +0100

    Added json data and main portfolio display.

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,5 +1,15 @@
+import os
+import json
+
+from django.conf import settings
 from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'main.html')
+
+    file_path = os.path.join(settings.BASE_DIR, 'data', 'portfolio.json')
+    with open(file_path, 'r', encoding='utf-8') as file:
+        portfolio_data = json.load(file)
+
+    return render(request, 'main.html',
+                  {'portfolio_data': portfolio_data})
